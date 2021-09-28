f8fc9e1f38f205815dee4ddd9d185f90bdcfa164
Brady Williams <williams.cole29@yahoo.com>
2021-09-28 15:36:28 -0500

    Added the isAdmin functionality to gain more understanding of the assignment

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { PropsAndState } from "./PropsAndState";
 
-export const Home = () => (
-    <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
+export const Home = ({isAdmin}) => {
+         return <>
+            { isAdmin
+            ? <> <h2>Nashville Kennels</h2>
+            <small>Loving care when you're not there.</small>
+    
+            <address>
+                <div>Visit Us at the Nashville North Location</div>
+                <div>500 Puppy Way</div>
+            </address> 
+            <PropsAndState yourName={"Brady"} />
+            </>
+            : <p>Not an Admin</p>  
+            } 
 
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-        <PropsAndState yourName={"Brady"} />
-    </>
-)
+         </>
+    }
