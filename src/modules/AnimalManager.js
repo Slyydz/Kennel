01be9c5c54e8bb1a4c961ba79dc5597caac0f8cc
Brady Williams <williams.cole29@yahoo.com>
2021-09-28 14:11:32 -0500

    added authentication

--- src/modules/AnimalManager.js
+++ src/modules/AnimalManager.js
@@ -1,5 +1,6 @@
 const remoteURL = "http://localhost:5002"
 
+
 export const getAnimalById = (animalId) => {
   //be sure your animals have good data and related to a location and customer
   return fetch(`${remoteURL}/animals/${animalId}`)
